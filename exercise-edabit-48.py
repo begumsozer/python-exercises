48e8caa429ebe9f825d6052d99a79db3070274c6
Begum Sozer <55942612+begumsozer@users.noreply.github.com>
2020-01-19 13:08:56 +0300

    Update exercise-edabit-48.py

--- exercise-edabit-48.py
+++ exercise-edabit-48.py
@@ -1,24 +1,16 @@
 def chosen_wine(wines):
-  if len(wines)==0:
+ if len(wines)==0:
     return None
   elif len(wines)==1:
     return wines[0]["name"]
   prices=[]
   for x in wines:
     prices.append(x["price"])
-  for x in range(len(wines)):
-    if wines[x]["price"]==min(prices):
-      wines[x]=0
-      break
-  prices=[]
+  prices.sort()
   for x in wines:
-    if x!=0:
-      prices.append(x["price"])
-  for x in range(len(wines)):
-    if wines[x]!=0:
-      if wines[x]["price"]==min(prices):
-        return wines[x]["name"]
-
+    if x["price"]==prices[1]:
+     return x["name"]
+   
 print(chosen_wine([
   { "name": "Wine A", "price": 8.99 },
   { "name": "Wine 32", "price": 13.99 },
